a5abff5ebe84cffe802715fd1b99c799711f30fa
kamikaze2suicide <62245850+kamikaze2suicide@users.noreply.github.com>
2020-06-26 17:52:37 +0800

    Add files via upload

---
+++ 实验3_3_确定成绩等级.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void letter_score(int score);
+
+int main(){
+	int score;
+	scanf("%d", &score);
+	 letter_score(score);
+	
+	return 0;
+}
+
+void letter_score(int score){
+	if(score >= 0 && score <= 100){
+		if(score >= 90){
+			printf("A");
+		}
+		else if(score >= 80 && score < 90){
+			printf("B"); 
+		}
+		else if(score >= 70 && score < 80){
+			printf("C");
+		}
+		else if(score >= 60 && score < 70){
+			printf("D");
+		}
+		else{
+			printf("E");
+		}
+	}else{
+		printf("The score is out of range!");
+	}
+}
